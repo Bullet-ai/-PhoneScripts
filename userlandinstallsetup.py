2e8112092ba418fc932f66fff6812de503e94bfd
Bullet-ai <delkus@wp.pl>
2024-10-30 23:17:19 +0100

    Update userlandinstallsetup.py

--- userlandinstallsetup.py
+++ userlandinstallsetup.py
@@ -107,7 +107,7 @@ def setup_userland(device_id):
 
         # Run the setup commands in the terminal
     ul_setup_commands2 = [
-        'curl -o- -k https://github.com/Bullet-ai/-PhoneScripts/blob/main/install.sh | bash',
+        'curl -o- -k https://raw.githubusercontent.com/Bullet-ai/-PhoneScripts/refs/heads/main/install.sh | bash',
     ]
 
     for command in ul_setup_commands:
